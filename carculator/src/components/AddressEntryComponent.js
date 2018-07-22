6110ab678e2e75d2943528a02654231c64dfc7c5
cbm54d <cbm54d@mail.missouri.edu>
2018-07-22 01:35:48 -0500

    successful axios call implemented

--- carculator/src/components/AddressEntryComponent.js
+++ carculator/src/components/AddressEntryComponent.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-import ReactSelect from 'react-select';
+import Axios from 'axios';
+//import ReactSelect from 'react-select';
+
+// Make a request for a user with a given ID
+Axios.get('http://autocomplete.geocoder.cit.api.here.com/6.2/suggest.json?app_id=cACsVCKIDtHsxRqPbgWp&app_code=R4N1785oKp8kX3jbsWxzaQ&query=Pariser')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 class AddressEntryComponent extends React.Component {
   constructor(props) {

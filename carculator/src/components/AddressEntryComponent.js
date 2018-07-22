8223806d903e6422f5bff0ba2815ee455fc69a3a
cbm54d <cbm54d@mail.missouri.edu>
2018-07-22 02:05:35 -0500

    mild cleanup

--- carculator/src/components/AddressEntryComponent.js
+++ carculator/src/components/AddressEntryComponent.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
 import Axios from 'axios';
 import {Async} from 'react-select';
 import 'react-select/dist/react-select.css';
-
-
-
-// Make a request for a user with a given ID
-//Axios.get('http://autocomplete.geocoder.cit.api.here.com/6.2/suggest.json?app_id=cACsVCKIDtHsxRqPbgWp&app_code=R4N1785oKp8kX3jbsWxzaQ&query=Pariser')
-//  .then(function (response) {
-//    console.log(response);
-//  })
-//  .catch(function (error) {
-//    console.log(error);
-//  });
 
 class AddressEntryComponent extends React.Component {
   handleClick() {
@@ -30,21 +18,9 @@ class AddressEntryComponent extends React.Component {
          "&app_code=R4N1785oKp8kX3jbsWxzaQ" +
          "&query=" + input;
 
-//        var settings = {
-//              "async": true,
-//              "crossDomain": true,
-//              "url": url,
-//              "method": "GET",
-//              "headers": {
-//                "Cache-Control": "no-cache",
-//                "Postman-Token": "e1930da5-6731-4611-82f1-7121478d84b6"
-//              }
-//        }
-
         Axios.get(url).then(function (response) {
           console.log(response);
-            if (Object.keys(response).length === 0 && response.constructor === Object
-){
+            if (Object.keys(response).length === 0 && response.constructor === Object){
                 throw "empty response";
             }
             var data = response.data;
